f117d4ca262b186aac72bb528f941d8fc0c0a557
Abalagu <xuluming95@gmail.com>
2019-09-23 13:24:36 -0500

    auto format all code

--- Assignment_2/client.c
+++ Assignment_2/client.c
@@ -1,10 +1,10 @@
 #include "headers.h"
 #include "config.h"
 
-
-
-int main (int argc, char * argv[]) { 
-    if (argc != 4) {       
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
         fprintf(stderr, "Usage: client username server_ip server_port\n");
         exit(1);
     }

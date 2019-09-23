bacfa7c627b16787c21c5785fa7549e943933d1d
Abalagu <xuluming95@gmail.com>
2019-09-23 12:47:41 -0500

    extract typedefs to config.h

--- Assignment_2/client.c
+++ Assignment_2/client.c
@@ -1,34 +1,5 @@
 #include "headers.h"
-
-
-typedef enum sbcpheadertypes_t {
-    JOIN = 2, 
-    FWD = 3, 
-    SEND = 4
-} headerType;
-
-
-typedef enum sbcpattributetype_t {
-    REASON = 1,
-    USERNAME = 2,
-    CLIENTCOUNT = 3,
-    MESSAGE = 4
-} attributeType;
-
-
-typedef struct scbpattribute_t {
-    attributeType type;
-    int len;
-    int payload;
-} sbcpAttribute;
-
-
-typedef struct sbcpmsg_t {
-    int vrsn;                       // protocol version
-    headerType type;                // SBCP message type
-    int len;                        // length of SBCP message
-    sbcpAttribute payload;          // payload 
-} sbcpMsg;
+#include "config.h"
 
 
 
